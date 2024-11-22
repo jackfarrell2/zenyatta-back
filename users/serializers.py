68ba05bd9ca1e4f0ba899ff1b7d66b3c714a1abc
Jack <jackfarrell860@gmail.com>
2024-11-21 21:07:01 -0500

    updated requirements

--- users/serializers.py
+++ users/serializers.py
@@ -5,6 +5,8 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
+    # test
+
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password')
