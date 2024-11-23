7923e724b262300319a48074b9c5a1ea69aa1c1c
Jack <jackfarrell860@gmail.com>
2024-11-23 11:30:34 -0500

    worked on example node

--- users/serializers.py
+++ users/serializers.py
@@ -5,8 +5,6 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
-    # test
-
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password')
